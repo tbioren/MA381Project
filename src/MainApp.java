82f13e792072d47edd915992adb9f41cc8c812bd
Canon <cm090.tech@gmail.com>
2023-05-21 11:40:53 -0400

    Stuff

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,22 +1,31 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 class MainApp {
     public static void main(String[] args) throws IOException {
-        FileWriter fw = new FileWriter("output.csv");
-        Generation g = new Generation(100, 1200, 0.001, false, 10, SelectionMethod.BEST_RANDOM_WORST);
-        String prevOut = "";
-        for(int i=0; i < 1000; i++) {
-            for(int j=0; j < prevOut.length(); j++) {
-                System.out.print("\b");
+        FileWriter fw;
+        SelectionMethod[] methods = { SelectionMethod.TRUNCATION, SelectionMethod.ROULETTE, SelectionMethod.RANK,
+                SelectionMethod.BEST_RANDOM_WORST };
+        for (SelectionMethod method : methods) {
+            for (int iteration = 0; iteration < 10; iteration++) {
+                new File(String.format("output/%s-trial%d.csv", method, 1)).createNewFile();
+                fw = new FileWriter(String.format("output/%s-trial%d.csv", method, iteration));
+                Generation g = new Generation(100, 1200, 0.001, false, 10, method);
+                String prevOut = "";
+                for (int i = 0; i < 1000; i++) {
+                    for (int j = 0; j < prevOut.length(); j++) {
+                        System.out.print("\b");
+                    }
+                    prevOut = "Generation " + i + ": Average Fitness: " + g.getAverageFitness();
+                    System.out.print(prevOut);
+                    fw.append(i + "," + g.getBestFitness() + "\n");
+                    g.evolve();
+                }
+                fw.close();
+                System.out.println();
+                System.out.println("Best Fitness: " + g.getBestFitness());
             }
-            prevOut = "Generation " + i + ": Average Fitness: " + g.getAverageFitness();
-            System.out.print(prevOut);
-            fw.append(i + "," + g.getBestFitness() + "\n");
-            g.evolve();
         }
-        fw.close();
-        System.out.println();
-        System.out.println("Best Fitness: " + g.getBestFitness());
     }
 }
